284ee34d744504c2ad059c81340582a4bde60831
Aaron Bermingham <aaron.bermingham@travelport.com>
2023-06-14 15:15:07 +0100

    create action file

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+// actions
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET = 'SET';
+
+// action creators
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
+export const set = (value) => ({ type: SET, payload: value });
