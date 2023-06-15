0d493f177b55ed0605386d371b9120a8c7a56f98
Aaron Bermingham <aaron.bermingham@travelport.com>
2023-06-15 13:28:55 +0100

    create `useCounter` hook, handles actions there

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { set, increment, decrement } from './actions';
 import { SetCounter } from './SetCounter';
-import { useActions } from './useActions';
+import { useCounter } from './useCounter';
 
 export const Counter = () => {
   const incident = 'Incident';
   // get state from store using useSelector hook
-  const count = useSelector((state) => state.count);
-  const dispatch = useDispatch();
+  // const count = useSelector((state) => state.count);
+  // const dispatch = useDispatch();
 
   /* refactor to avoid calling dispatch on each button onClick, returns an object whose 
    values are action creators, into an object with the same keys, but with every action 
@@ -16,20 +13,22 @@ export const Counter = () => {
   //const actions = bindActionCreators({ increment, decrement, set }, dispatch);
 
   // same as above but using useActions custom hook
-  const actions = useActions({
-    increment,
-    decrement,
-    set
-  });
+  // const actions = useActions({
+  //   increment,
+  //   decrement,
+  //   set
+  // });
+
+  const { count, increment, decrement, set } = useCounter();
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => actions.increment()}>Increment</button>
-        <button onClick={() => actions.set(0)}>Reset</button>
-        <button onClick={() => actions.decrement}>Decrement</button>
+        <button onClick={() => increment()}>Increment</button>
+        <button onClick={() => set(0)}>Reset</button>
+        <button onClick={() => decrement}>Decrement</button>
       </section>
       <SetCounter />
     </main>
