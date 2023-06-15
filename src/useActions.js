0ff22cfaf51a476c6e73705236afb2746e9d7e88
Aaron Bermingham <aaron.bermingham@travelport.com>
2023-06-15 13:40:28 +0100

    add useMemo hook to `useAction` hook

--- src/useActions.js
+++ src/useActions.js
@@ -1,9 +1,13 @@
 // hook to handle dispatching actions
+import { useMemo } from 'react';
 
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 export const useActions = (actions) => {
   const dispatch = useDispatch();
-  return bindActionCreators(actions, dispatch);
+  return useMemo(
+    () => bindActionCreators(actions, dispatch),
+    [actions, dispatch]
+  );
 };
